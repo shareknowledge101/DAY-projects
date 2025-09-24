d322d1e84e2fe13137d1bbb3186aa52a6b8ddeb0
shareknowledge101 <demonsslayer190@gmail.com>
2025-09-24 14:41:03 -0400

    Added a modification to project_3

--- DAY-2/projet_3.c
+++ DAY-2/projet_3.c
@@ -2,23 +2,67 @@
 
 int main()
 {
-    int possible_d_heure[] = {480, 583, 679, 767, 840, 945, 1020, 1305};
-    int heure, minutes, formula;
+    // jz ajoute un conversionn Heures de départ en minutes
+    int depart_times[] = {
+        8 * 60 + 0,    // 8:00
+        9 * 60 + 43,   // 9:43
+        11 * 60 + 19,  // 11:19
+        12 * 60 + 47,  // 12:47
+        14 * 60 + 0,   // 14:00
+        15 * 60 + 45,  // 15:45
+        19 * 60 + 0,   // 19:00
+        21 * 60 + 45   // 21:45
+    };
 
-    //i add a loop to check input of user
+    // conversion des Heures d'arrivée correspondantes en minutes 
+    int arrival_times[] = {
+        10 * 60 + 16,  // 10:16
+        11 * 60 + 52,  // 11:52
+        13 * 60 + 31,  // 13:31
+        15 * 60 + 0,   // 15:00
+        16 * 60 + 8,   // 16:08
+        17 * 60 + 55,  // 17:55
+        21 * 60 + 20,  // 21:20
+        23 * 60 + 58   // 23:58
+    };
 
-    printf("\nEntrer l'heure de depart : ");
+    int heure, minutes, user_time;
+    int i, min_diff = 99999; // pour eviter le conflit en plus de marke un point de depart numerique
+    int closest_index = 0;
 
+    printf("\nEntrer l'Heure de depart : ");
     scanf("%d", &heure);
 
-    printf("\nEntrer minutes de depart : ");
-    
+    printf("\nEntrer Minutes de depart : ");
     scanf("%d", &minutes);
 
-    formula = (heure * 60) + minutes;
+    user_time = heure * 60 + minutes;
 
-    printf("Your approximate time is : %d\n", formula);
+    printf("Heure saisie en minutes : %d\n", user_time);
 
-    //
+    // Cherche l'indice du vol le plus proche
+    for (i = 0; i < 8; i++) {
 
+        int diff = depart_times[i] - user_time;
+
+        if (diff < 0) diff = -diff;
+
+        if (diff < min_diff) {
+            min_diff = diff;
+            closest_index = i;
+        }
+    }
+
+    int dep_heure = depart_times[closest_index] / 60;
+
+    int dep_min = depart_times[closest_index] % 60;
+
+    int arr_hour = arrival_times[closest_index] / 60;
+    
+    int arr_min = arrival_times[closest_index] % 60;
+
+    printf("Le vol le plus proche part à : %02d:%02d\n", dep_heure, dep_min);
+    printf("Heure d'arrivée correspondante : %02d:%02d\n", arr_hour, arr_min);
+
+    return 0;
 }
